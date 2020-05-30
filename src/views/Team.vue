1ca8e9f90875aac603c1e1f6e55a1f874922683f
ClementLong <clmt.long@gmail.com>
2020-05-30 17:07:45 +0200

    add team demo

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an team page</h1>
+    <div v-for="(member, index) in members" :key="index">
+      {{ member }}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      members: [
+        'Clément',
+        'Bastien'
+      ]
+    }
+  }
+}
+</script>
