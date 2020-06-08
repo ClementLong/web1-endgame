ac6f018fe7b33223c2582efad050fd849155f2b3
anthony-roux <roux.anthony@hetic.net>
2020-06-08 20:03:30 +0200

    fix conflict in home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="home">
     <HelloWorld msg="Les coraux, la vie se meurt" />
-    <DisclaimerArticle text="Ce site a été réalisé à des fins pédagogiques dans le cadre du cursus Bachelor de l’école HETIC.
+    <DisclaimerArticle
+      text="Ce site a été réalisé à des fins pédagogiques dans le cadre du cursus Bachelor de l’école HETIC.
     Les contenus présentés n'ont pas fait l'objet d'une demande de droit d'utilisation.
-    Ce site ne sera en aucun cas exploité à des fins commerciales et ne sera pas publié."/>
+    Ce site ne sera en aucun cas exploité à des fins commerciales et ne sera pas publié."
+    />
+    <ChapterTitleAndReturn
+      currentChapter="01"
+      chapter="07"
+      title="Mais que ce passe t-il ?"
+      reviewChapter="revoir le chapitre précédent"
+    />
   </div>
 </template>
 
@@ -11,13 +19,14 @@
 // @ is an alias to /src
 import DisclaimerArticle from "@/components/Frame-disclaimer_article.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import ChapterTitleAndReturn from "@/components/ChapterTitleAndReturn.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
-    DisclaimerArticle
+    DisclaimerArticle,
+    ChapterTitleAndReturn
   }
 };
 </script>
