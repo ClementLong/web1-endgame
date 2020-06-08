d35848959015f2fb1bfc5d76e25976efb01aae5e
anthony-roux <roux.anthony@hetic.net>
2020-06-08 19:52:49 +0200

    import ChapterTitleAndReturn component in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="home">
     <HelloWorld msg="Les coraux, la vie se meurt" />
+    <ChapterTitleAndReturn
+      currentChapter="01"
+      chapter="07"
+      title="Mais que ce passe t-il ?"
+      reviewChapter="revoir le chapitre précédent"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import ChapterTitleAndReturn from "@/components/ChapterTitleAndReturn.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    ChapterTitleAndReturn
   }
 };
 </script>
