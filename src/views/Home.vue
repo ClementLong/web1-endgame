ff642e07d3bfe9ade0a72a22d3cd0409251cb023
annie.trn <annietth@users.noreply.github.com>
2020-06-09 10:34:42 +0200

    fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld msg="Les coraux, la vie se meurt" />
+    <NextChapterButton msg="PASSEZ AU CHAPITRE SUIVANT" /> 
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import NextChapterButton from "@/components/next-chapter_button.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    NextChapterButton
   }
 };
 </script>
