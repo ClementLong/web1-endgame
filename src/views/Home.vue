a93bf6848117a0c05213b8d5742722fd9179f590
Aymeric-Gillet <aymeric.gillet@hetic.net>
2020-06-09 09:27:38 +0200

    add img :camera:

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="home">
     <img alt="aerosmith" src="https://images6.alphacoders.com/410/thumb-1920-410779.jpg" />
     <HelloWorld msg="Groupe 11" />
-    <h2 msg ="RAP / ROCK" />
+    <p>{{NomDuProjet ="RAP / ROCK"}}</p>
+    <h1>Bonjour</h1>
   </div>
 </template>
 
@@ -24,6 +25,10 @@ export default {
   img {
   width: 1200px;
   height: 650px;
+}
+
+p {
+  color: red;
 }
 
 </style>
