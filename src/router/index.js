050471775f8b83152837c1c8b03902b8de666ba0
unknown <pierre-alexis.maerten@getic.net>
2020-06-12 17:51:12 +0200

    comments

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,7 @@ Vue.use(VueRouter)
 
 // C'est dans la constante routes que vous définisez les différentes routes de votre projet
 // Chaque route doivent avoir au minimum un path et un component associé
+// blabla
 const routes = [{
     path: '/',
     name: 'Home',
