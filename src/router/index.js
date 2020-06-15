4b166c500c35b5f502312a33e316e3d4bdcab9e7
pierre-alexism <pierre-alexis@hetic.net>
2020-06-15 17:58:48 +0200

    making a chapter component with dynamic props

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue';
-import Team from '../views/Team.vue';
-import Intro from '../views/Intro.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Team from "../views/Team.vue";
+import Intro from "../views/Intro.vue";
 import Chapitre0 from "@/views/Chapitre0.vue";
 import Chapitre1 from "@/views/Chapitre1.vue";
 import Chapitre2 from "@/views/Chapitre2.vue";
@@ -20,127 +20,135 @@ import Quizz3 from "@/views/Quizz3.vue";
 import Quizz4 from "@/views/Quizz4.vue";
 import Quizz5 from "@/views/Quizz5.vue";
 import Assos from "@/views/Assos.vue";
+import Chapter from "@/views/Chapter.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 // C'est dans la constante routes que vous définisez les différentes routes de votre projet
 // Chaque route doivent avoir au minimum un path et un component associé
 // blabla
-const routes = [{
-    path: '/',
-    name: 'Home',
-    component: Home
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/team',
-    name: 'Team',
-    component: Team
+    path: "/team",
+    name: "Team",
+    component: Team,
   },
   {
-    path: '/intro',
-    name: 'Intro',
-    component: Intro
-  }, {
-    path: '/chap0',
-    name: 'Chapitre0',
-    component: Chapitre0
+    path: "/intro",
+    name: "Intro",
+    component: Intro,
   },
   {
-    path: '/chap1',
-    name: 'Chapitre1',
+    path: "/chap0",
+    name: "Chapitre0",
+    component: Chapitre0,
+  },
+  {
+    path: "/chap1",
+    name: "Chapitre1",
     component: Chapitre1,
-    children: [{
-
-      path: 'article',
-      name: 'Chapitre1Article1',
-      component: ArticlePicture1
-      },{
-
-      path: 'quizz',
-      name: 'Chapitre1Quizz1',
-      component: Quizz1
-      }
-    ]
+    children: [
+      {
+        path: "article",
+        name: "Chapitre1Article1",
+        component: ArticlePicture1,
+      },
+      {
+        path: "quizz",
+        name: "Chapitre1Quizz1",
+        component: Quizz1,
+      },
+    ],
   },
   {
-    path: '/chap2',
-    name: 'Chapitre2',
+    path: "/chap2",
+    name: "Chapitre2",
     component: Chapitre2,
-    children: [{
-
-      path: 'article',
-      name: 'Chapitre2Article2',
-      component: ArticlePicture2
-      },{
-
-      path: 'quizz',
-      name: 'Chapitre2Quizz2',
-      component: Quizz2
-      }
-    ]
+    children: [
+      {
+        path: "article",
+        name: "Chapitre2Article2",
+        component: ArticlePicture2,
+      },
+      {
+        path: "quizz",
+        name: "Chapitre2Quizz2",
+        component: Quizz2,
+      },
+    ],
   },
   {
-    path: '/chap3',
-    name: 'Chapitre3',
+    path: "/chap3",
+    name: "Chapitre3",
     component: Chapitre3,
-    children: [{
-
-      path: 'quizz',
-      name: 'Chapitre3Quizz3',
-      component: Quizz3
-      }
-    ]
+    children: [
+      {
+        path: "quizz",
+        name: "Chapitre3Quizz3",
+        component: Quizz3,
+      },
+    ],
   },
   {
-    path: '/chap4',
-    name: 'Chapitre4',
+    path: "/chap4",
+    name: "Chapitre4",
     component: Chapitre4,
-    children: [{
-
-      path: 'article',
-      name: 'Chapitre4Article4',
-      component: ArticlePicture4
-      },{
-
-      path: 'quizz',
-      name: 'Chapitre4Quizz4',
-      component: Quizz4
-      }
-    ]
+    children: [
+      {
+        path: "article",
+        name: "Chapitre4Article4",
+        component: ArticlePicture4,
+      },
+      {
+        path: "quizz",
+        name: "Chapitre4Quizz4",
+        component: Quizz4,
+      },
+    ],
   },
   {
-    path: '/chap5',
-    name: 'Chapitre5',
+    path: "/chap5",
+    name: "Chapitre5",
     component: Chapitre5,
-    children: [{
-
-      path: 'article',
-      name: 'Chapitre5Article5',
-      component: ArticlePicture5
-      },{
-
-      path: 'quizz',
-      name: 'Chapitre5Quizz5',
-      component: Quizz5
-      }
-    ]
+    children: [
+      {
+        path: "article",
+        name: "Chapitre5Article5",
+        component: ArticlePicture5,
+      },
+      {
+        path: "quizz",
+        name: "Chapitre5Quizz5",
+        component: Quizz5,
+      },
+    ],
   },
   {
-    path: '/chap6',
-    name: 'Chapitre6',
-    component: Chapitre6
+    path: "/chap6",
+    name: "Chapitre6",
+    component: Chapitre6,
   },
   {
-    path: '/assos',
-    name: 'Assos',
-    component: Assos
-  }
-]
+    path: "/assos",
+    name: "Assos",
+    component: Assos,
+  },
+  {
+    path: "/chapter",
+    name: "Chapter",
+    component: Chapter,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
