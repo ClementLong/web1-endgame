a2a5d6d32b2c890cb9ff59107742142fbb853719
Mauduit-O <oum-el-khaire.mauduit@hetic.net>
2020-06-16 02:13:39 +0200

    update texte description in all views

--- src/views/Hongi.vue
+++ src/views/Hongi.vue
@@ -15,9 +15,9 @@ export default {
   data() {
     return {
       // img: require("../assets/img/backgroundHongi.svg"),
-      title: "Le hongi",
+      title: "Hongi",
       description:
-        "Le hongi est le salut traditionnel maori, l’origine de ce geste remonterait à la création de l'humanité.",
+        "Le hongi est le salut traditionnel maori, l’origine de ce geste remonterait à la création de l'humanité. À la création de la première femme, Hine ahu one ' femme faite de terre ', par le dieu créateur Tane, celui-ci l'aurait fabriquée avec de la glaise avant de lui insuffler la vie par les narines. D'où cet échange de souffle appelé ' ha ', ou le souffle de la vie.Aujourd’hui, cette pratique est utilisée par des membres du gouvernement ou de la famille royale lors de rencontres officielles.",
     };
   },
 };
