75fd4854d33283ffcc0ade62d45c3d3b195f9618
gravity-zero <gravity.neo@gmail.com>
2020-06-18 14:57:52 +0200

    fixed version

--- src/views/familles/Mammiferes.vue
+++ src/views/familles/Mammiferes.vue
@@ -15,11 +15,28 @@
 
 <script>
 import headerFamilles from "@/components/partials/headerFamilles";
+import axios from "axios";
 
 export default {
   name: "mammiferes",
   components: {
     headerFamilles
+  },
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        "https://cors-anywhere.zelazna.fr/https://pal.romain-feregotto.fr/?url=list",
+        {
+          method: "GET",
+          headers: { Authorization: "Basic " + btoa("admin:admin") }
+        }
+      )
+      .then(response => (console.log(response.data.results.species.filter( f => f.deep_min >= this.$route.params.deep_min && f.deep_max <= this.$route.params.deep_max))));
   }
 };
 </script>
